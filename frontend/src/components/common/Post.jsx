9ddf1c358598c706bbbb99f7e200f291f9d72fd9
Atul Soam <atul.soam@hcl.com>
2024-04-27 10:47:49 +0530

    create post

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,25 +5,56 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query"
+import { toast } from "react-hot-toast"
+import LoadingSpinner from "./LoadingSpinner"
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const postOwner = post.user;
 	const isLiked = false;
+	const queeryclient = useQueryClient()
 
-	const isMyPost = true;
+	const { data } = useQuery({ queryKey: ["authuser"] })
+
+	const { mutate: deletPost, isPending } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`api/post/${post._id}`,{
+					method:"DELETE"
+				})
+
+				const data = await res.json()
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong")
+				}
+				return data
+			} catch (error) {
+				throw new Error(error.message)
+			}
+		},
+		onSuccess:()=>{
+			toast.success("Post deleted succesfully!! ")
+			queeryclient.invalidateQueries({queryKey:["posts"]})
+		}
+
+	})
+
+	const isMyPost = data._id === post.user._id;
 
 	const formattedDate = "1h";
 
 	const isCommenting = false;
 
-	const handleDeletePost = () => {};
+	const handleDeletePost = () => {
+		deletPost()
+	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => { };
 
 	return (
 		<>
@@ -45,7 +76,10 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+								{!isPending &&
+									<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+}
+								{isPending && <LoadingSpinner size="sm"/>}
 							</span>
 						)}
 					</div>
@@ -135,9 +169,8 @@ const Post = ({ post }) => {
 								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
-									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-										isLiked ? "text-pink-500" : ""
-									}`}
+									className={`text-sm text-slate-500 group-hover:text-pink-500 ${isLiked ? "text-pink-500" : ""
+										}`}
 								>
 									{post.likes.length}
 								</span>
--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -2,23 +2,53 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query"
+import { toast } from "react-hot-toast"
+// import LoadingSpinner from "./LoadingSpinner"
 
 const CreatePost = () => {
 	const [text, setText] = useState("");
 	const [img, setImg] = useState(null);
 
 	const imgRef = useRef(null);
+	const queeryclient = useQueryClient()
+	const { data: user } = useQuery({ queryKey: ["authuser"] })
 
-	const isPending = false;
-	const isError = false;
+	const { mutate: createPost, isPending, isError } = useMutation({
+		mutationFn: async ({ text, img }) => {
+			try {
+				const res = await fetch(`api/post/create`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({ text, img })
+				}
 
-	const data = {
-		profileImg: "/avatars/boy1.png",
-	};
+				)
+
+				const data = await res.json()
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong")
+				}
+				return data
+			} catch (error) {
+				throw new Error(error.message)
+			}
+		},
+		onSuccess: () => {
+			setText("")
+			setImg(null)
+			toast.success("Post uploaded succesfully!! ")
+			queeryclient.invalidateQueries({ queryKey: ["posts"] })
+		}
+
+	})
+
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		alert("Post created successfully");
+		createPost({ text, img })
 	};
 
 	const handleImgChange = (e) => {
@@ -36,7 +66,7 @@ const CreatePost = () => {
 		<div className='flex p-4 items-start gap-4 border-b border-gray-700'>
 			<div className='avatar'>
 				<div className='w-8 rounded-full'>
-					<img src={data.profileImg || "/avatar-placeholder.png"} />
+					<img src={user.profilepic || "/avatar-placeholder.png"} />
 				</div>
 			</div>
 			<form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit}>
