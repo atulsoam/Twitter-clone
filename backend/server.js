8f47cbe7538513f79787e28ba9e46fff5656f9b1
Atul Soam <atul.soam@hcl.com>
2024-04-29 22:45:05 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import path from "path"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
@@ -22,6 +23,8 @@ cloudinary.config({
 
 const app = express()
 
+const __dirname = path.resolve()
+
 app.use(express.json({limit:"5mb"}))
 
 app.use(cookieParser())
@@ -30,6 +33,11 @@ app.use("/api/auth",authrouter)
 app.use("/api/users",userRoute)
 app.use("/api/post",postroutes)
 app.use("/api/notification",NotificationRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*", (req,res)=>{
+  res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+})
 
 
 
